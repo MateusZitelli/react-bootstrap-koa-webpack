6ddda7a6282cf520e8d1d5822edcf2a569271564
Tom Chen <developer@tomchentw.com>
2014-09-25 11:22:15 +0800

    chore(package.json): use koa-webpack-dev as development layer

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
-var koa = require("koa");
 var Path = require("path");
+var http = require("http");
+
+var koa = require("koa");
+var koaStatic = require("koa-static");
+
 
 var IS_PRODUCTION = "production" === process.env.NODE_ENV;
 
 var app = koa();
 
-if (IS_PRODUCTION) {
-  app.use(require("koa-file-server")({
-    root: Path.resolve("../public"),
-    index: true
-  }));
+app.use(koaStatic(Path.resolve("./public")));
+
+if (!IS_PRODUCTION) {
+  var koaWebpackDev = require("koa-webpack-dev");
+  var webpackConfig = koaWebpackDev.configure({
+    inline: true
+  });
+
+  var compiler = require("webpack")(webpackConfig);
+  app.use(koaWebpackDev.middleware(compiler, webpackConfig.devServer));
 }
 
-// app.use(function *(){
-//   this.body = "Hello World";
-// });
+var server = http.createServer(app.callback());
 
-app.listen(3000);
+if (!IS_PRODUCTION) {
+  koaWebpackDev.hotModuleSocket(server, compiler);
+}
+
+server.listen(8080);
